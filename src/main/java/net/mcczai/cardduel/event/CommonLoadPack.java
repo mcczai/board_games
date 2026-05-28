a2b55c43f75c0eb34844a707185d2d4f811adb22
mcczai <hjcz903cyd@163.com>
2026-05-28 13:51:30 +0800

    修复mixin 第一次

--- src/main/java/net/mcczai/cardduel/event/CommonLoadPack.java
+++ src/main/java/net/mcczai/cardduel/event/CommonLoadPack.java
@@ -6,7 +6,7 @@ import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 
-@EventBusSubscriber(modid = CardduelMod.MODID,bus = EventBusSubscriber.Bus.MOD)
+@EventBusSubscriber(modid = CardduelMod.MODID)
 public class CommonLoadPack {
 
     @SubscribeEvent
